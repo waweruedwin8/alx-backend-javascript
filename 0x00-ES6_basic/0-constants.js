07df718afe746da4a9a96d512d1f4205a83a9323
waweruedwin8 <waweruedwin8@gmail.com>
2024-07-26 19:53:40 +0300

    const and let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -11,19 +11,4 @@ function getLast(){
 	return 'is Okey';
 }
 console.log('${taskFirst()} ${taskNext()} ${getLast()}');
-/* 
-export function taskFirst() {
-	const task = 'I prefer const when I can.';
-	return task;
-     }
-     
-     export function getLast() {
-	return ' is okay';
-     }
-     
-     export function taskNext() {
-	let combination = 'But sometimes let';
-	combination += getLast();
-	return combination;
-     }
-*/  
+
