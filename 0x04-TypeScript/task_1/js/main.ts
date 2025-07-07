77b1e9d7f43a3587a3622fc55716ca165fa75eef
waweruedwin8 <waweruedwin8@gmail.com>
2025-07-07 21:29:59 +0300

    added  functions

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,12 @@
 export interface Teacher{
-    readonly firstName: string;
+    readonly firstName: string; // an attribute that can only be changed once during initialization
     readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
-    [anyVar: string]: any;
+    [anyVar: string]: any; // an attribute that is not specified 
 }
-
+// an interface that inherits from teacher interface
 export interface Directors extends Teacher{
   numberOfReports: number;
     
@@ -29,3 +29,13 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+//
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("Edwin", "Waweru")); // E. Waweru
